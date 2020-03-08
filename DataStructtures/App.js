9a74c7b6ddde59a540ef8f610919ecca215d54fb
andre-silva-14 <silva.andre1997@gmail.com>
2020-03-08 08:04:42 +0100

    Test Script Update.

--- DataStructtures/App.js
+++ DataStructtures/App.js
@@ -1,8 +1,10 @@
-let list = new DoublyLinkedList()
+let list = new DoublyLinkedList(0,1,2);
 
-let test = [7,8,9]
-list.push(0,1,2,3,4,5,6)
-list.push(test)
+let arr1 = [5,6];
+let arr2 = [7,8,9];
+
+list.push(3,4);
+list.push(arr1, arr2);
 
 console.log(list);
 console.log(list.get(0));
@@ -12,3 +14,5 @@ console.log(list.get(8));
 console.log(list.get(-1));
 
 console.log(list.set("test",3));
+
+console.log(list.toArray());
