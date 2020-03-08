guilherme1100/AlgorithmsAndDataStructures

commit 71bd4eea768b469bf70f885ec1fedf99123b8bba

added insert method to doubly linked list

DataStructtures/doublyLinkedList.js
@@ -97,11 +97,29 @@ class DoublyLinkedList {
 
     set(val, index) {
         let selectedNode = this.get(index);
-        if (selectedNode) {
-            selectedNode.val = val;
-            return true;
-        }
-        return false;
+        if (selectedNode) return false;
+        selectedNode.val = val;
+        return true;
+    }
+
+    insert(val, index) {
+        if (index < 0 || index >= this.length) return false;
+        if (index === 0) return !!this.unshift(val);
+        if (index === this.length) return !!this.push(val);
+
+        let selectedNode = this.get(index);
+        let previeousNode = selectedNode.prev
+        let newNode = new Node(val);
+
+        previeousNode.next = newNode;
+        selectedNode.prev = newNode;
+        newNode.prev = previeousNode;
+        newNode.next = selectedNode;
+        
+        this.length++;
+
+        return true;
+
     }
 
 }

commit 7f2269be3e7b16e4b01aaf14ccf503ecb3719e61

DoublyLinkedList.set() Method Update:
- Method was broken after commit 
71bd4eea768b469bf70f885ec1fedf99123b8bba.
- Critical Bug where Method was always returning "false" or "index out 
of range" has been fixed.
- Method documentation has been updated.

DataStructtures/doublyLinkedList.js
@@ -235,14 +235,21 @@ class DoublyLinkedList {
         }
         return currentNode;
     }
-    
 
+
+    /**
+     * Changes the value of the node at the specified Index.
+     * @param {irrelevant} val - The new value to set, any data format accepted.
+     * @param {number} index - Index of the Node desired to change.
+     * @returns {boolean} - Returns true if opreation is successful and
+     * vice-versa.
+     */
     set(val, index) {
         let selectedNode = this.get(index);
-        if (selectedNode) return false;
-        selectedNode.val = val;
-        return true;
+        if (!!selectedNode) selectedNode.val = val; return true;
+        return false;
     }
+
 
     insert(val, index) {
         if (index < 0 || index >= this.length) return false;